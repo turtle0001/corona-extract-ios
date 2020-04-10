63ead60681018611197f3fb5527e3f0a9636a2bc
Joash Tubaga <joashtubaga@gmail.com>
2020-04-11 00:18:21 +0800

    [🎉] Initial commit

---
+++ corona-extract-ios/corona-extract-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  corona-extract-ios
+//
+//  Created by Joash Tubaga on 4/11/20.
+//  Copyright © 2020 Joash Tubaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
